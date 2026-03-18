b570e5ff2f51759912333e536aea01e940515cc0
Aryan jaiswal <aryan.47689.srmu@gmail.com>
2026-03-18 23:34:30 +0530

    Add files via upload

---
+++ 01_variables.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main() 
+{
+ int a; // variable declaration
+a = 10; // variable initialization
+ printf("The value of a is: %d\n", a); // printing the value of variable a
+    return 0;
+}
