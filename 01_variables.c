af4071f2556130202275b2d466ab087b1f80abfc
Aryan jaiswal <aryan.47689.srmu@gmail.com>
2026-03-18 23:38:40 +0530

    Delete 01_variables.c

--- 01_variables.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-int main() 
-{
- int a; // variable declaration
-a = 10; // variable initialization
- printf("The value of a is: %d\n", a); // printing the value of variable a
-    return 0;
-}
