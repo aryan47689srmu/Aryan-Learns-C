49c4149c93661b66f83c674e0ed9f3f83c9bf6bc
Aryan jaiswal <aryan.47689.srmu@gmail.com>
2026-03-18 23:46:17 +0530

    Create Basics/01_variales.c
    Added a C program that demonstrates variable declaration and initialization.

---
+++ Basics/01_variales.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main() 
+{
+ int a; // variable declaration
+a = 10; // variable initialization
+ printf("The value of a is: %d\n", a); // printing the value of variable a
+    return 0;
+}
+
+
+/*
+what is a variable?
+A variable is a named storage location in memory that can hold a value. It is used to store data that can be manipulated and accessed throughout a program. Variables have a specific data type that determines the kind of value they can hold, such as integers, floating-point numbers, characters, etc. In C, you must declare a variable before using it, specifying its data type and name. You can also initialize a variable at the time of declaration or assign a value to it later in the program. Variables are essential for storing and manipulating data in any programming language, including C.   
+*/
