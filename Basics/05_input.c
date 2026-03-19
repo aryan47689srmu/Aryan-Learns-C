5971d288d90a6ac36ad6bb3da7402635140aa7f6
Aryan jaiswal <aryan.47689.srmu@gmail.com>
2026-03-19 19:28:11 +0530

    Add user input and output functionality
    Implement a simple program to take user input and display it.

---
+++ Basics/05_input.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main()
+{
+    int a;
+    scanf("%d", &a);                      // taking input from the user and storing it in variable a
+    printf("The value of a is: %d\n", a); // printing the value of variable a
+    return 0;
+}
