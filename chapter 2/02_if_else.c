03738722cfa48489e359cf2cfff7cbe24981b679
Aryan jaiswal <aryan.47689.srmu@gmail.com>
2026-03-19 20:02:46 +0530

    Add if-else statement example in 02_if_else.c
    Implement if-else statement example in C

---
+++ chapter 2/02_if_else.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    int age = 10;
+
+    if (age > 10)
+    {
+        printf("We are inside if\n");
+        printf("Your age is greater than 10\n");
+    }
+    else
+    {
+        printf("We are inside else\n");
+        printf("Your age is less than or equal to 10\n");
+    }
+    return 0;
+}
+
+/*what is if else statement?
+An if-else statement is a programming construct that allows you to execute one block of code if a specified condition is true, and another block of code if the condition is false. It provides a way to make decisions in your code and control the flow of execution based on certain conditions. The syntax for an if-else statement in C is as follows: 
+*/
