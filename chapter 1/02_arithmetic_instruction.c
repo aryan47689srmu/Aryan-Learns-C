9fcb5266cd36087b035303bf333da3a5b4a62dbb
Aryan jaiswal <aryan.47689.srmu@gmail.com>
2026-03-19 19:50:21 +0530

    Add arithmetic instruction example in C

---
+++ chapter 1/02_arithmetic_instruction.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    int a = 10;
+    int b = 5;
+    int c = a / b;
+    printf("The value of a is %d and b is %d and quotient is %d", a, b, c);
+
+    return 0;
+}
+
+/*
+                + (Addition)
+                - (Subtraction)
+                * (Multiplication)
+                / (Division)
+                % (Modulus)   -:   oprator gives the remainder of the division of two numbers
+*/
