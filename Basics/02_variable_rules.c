5b24768a19f0c8a2f8441d91f3778365a72ae7a0
Aryan jaiswal <aryan.47689.srmu@gmail.com>
2026-03-19 19:15:38 +0530

    Add variable naming rules example in C

---
+++ Basics/02_variable_rules.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+    int aryan;    // This is valid because variable names can contain letters and can start with a letter.
+    int _aryan;   // This is valid because variable names can start with an underscore.
+    int aryan123; // This is valid because variable names can contain numbers, but cannot start with a number.
+    int Aryan;    // This is valid because variable names are case-sensitive, so 'Aryan' is different from 'aryan'.
+    return 0;
+}
