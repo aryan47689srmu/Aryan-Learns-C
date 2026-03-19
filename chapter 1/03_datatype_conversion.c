9eac9bb75aa3ebc8481f14f63fee3e69076d2bd7
Aryan jaiswal <aryan.47689.srmu@gmail.com>
2026-03-19 19:51:45 +0530

    Add datatype conversion example in C
    Implement a program to demonstrate datatype conversion in arithmetic operations.

---
+++ chapter 1/03_datatype_conversion.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    float a = 5;
+    int b = 2;
+    float c = a/b;
+    printf("The value of c is %f", c);
+    return 0;
+}
+
+
+
+/*
+An Arithmetic operation between 
+• int and int → int 
+• int and float → float 
+• float and float → float 
+*/
