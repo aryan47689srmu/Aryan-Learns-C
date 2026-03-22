e18229ceaa1c2e2a31cf6525ea11276f38ded051
Aryan jaiswal <aryan.47689.srmu@gmail.com>
2026-03-22 23:24:13 +0530

    Create 01_loop.c to print Happy Birthday
    Added a simple C program that prints 'Happy Birthday!' once.

---
+++ chapter 3/01_loop.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    // Problem: Print Happy Birthday 1 Lakh times
+    printf("Happy Birthday!\n"); 
+    return 0;
+}
+
+
+/*
+what is loop?
+A loop is a programming construct that allows you to repeat a block of code multiple times. It is used to perform repetitive tasks without having to write the same code over and over again. In C, there are three main types of loops: for loop, while loop, and do-while loop.       
+
+*/
