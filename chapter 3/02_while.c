c311e1f45178fb0a29319184e72f8bcc5e66969a
Aryan jaiswal <aryan.47689.srmu@gmail.com>
2026-03-22 23:25:36 +0530

    Add while loop example in 02_while.c
    Implement a while loop to print 'Happy birthday!' ten times.

---
+++ chapter 3/02_while.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    int i = 0;
+    while (i < 10)
+    {
+        printf("Happy birthday!\n");
+        i = i + 1;
+    }
+    return 0;
+}
+
+
+/*
+what is while loop?
+A while loop is a control flow statement that allows code to be executed repeatedly based on a given Boolean condition. The while loop can be thought of as a repeating if statement. The syntax of a while loop in C is as follows:        
+while (condition) {
+    // code to be executed
+}
+*/
