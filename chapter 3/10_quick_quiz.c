d6721ac1ac3dc4e5cfcaa7aec5b7c25c9e554514
Aryan jaiswal <aryan.47689.srmu@gmail.com>
2026-03-22 23:38:06 +0530

    Add program to print first 'n' natural numbers

---
+++ chapter 3/10_quick_quiz.c
@@ -0,0 +1,15 @@
+//Write a program to print first ‘n’ natural numbers using for loop.
+
+
+#include <stdio.h>
+
+int main()
+{
+    int n = 5;
+   for (int i = 1; i <= n; i++)
+   {
+    printf("%d\n", i);
+   }
+   
+    return 0;
+}
