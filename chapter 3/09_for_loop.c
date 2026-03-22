12f8b71a4912ff081e50c3bc2f7f116275bfeaff
Aryan jaiswal <aryan.47689.srmu@gmail.com>
2026-03-22 23:36:51 +0530

    Add for loop example in 09_for_loop.c
    Implement a for loop that prints the value of i.

---
+++ chapter 3/09_for_loop.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    int n = 6;
+    for (int i = 0; i < n; i++)
+    {
+        printf("The value of i is %d\n");
+    }
+    return 0;
+}
+
+/*
+What is for loop?
+A for loop is a control flow statement that allows code to be executed repeatedly based on a given Boolean condition. The for loop is typically used when the number of iterations is known beforehand. The syntax of a for loop in C is as follows:  
+for (initialization; condition; increment/decrement) {
+    // code to be executed
+}
+*/
